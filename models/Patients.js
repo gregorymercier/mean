47fa27c409c66f1c567832da5e607d0c5f87a801
gregorymercier <mercierg@gmai.com>
2016-12-12 22:11:02 +0100

    Patient Form improvement

--- models/Patients.js
+++ models/Patients.js
@@ -3,14 +3,14 @@ var mongoose = require('mongoose');
 var PatientSchema = new mongoose.Schema({
   lastname : String, //required
   firstname: String,
-  file : [{ fileid: String, filename : String}]
+  file : [{ fileid: String, filename : String}],
  // files : [{ type: mongoose.Schema.Types.ObjectId, ref: 'File'}] //https://github.com/aheckmann/gridfs-stream/issues/74
-  /*email:String,
+  email:String,
   phone:String,
   phone2:String,
   address: String,
   zipcode: String,
-  city: String*/
+  city: String
 });
 
 mongoose.model('Patient', PatientSchema);
